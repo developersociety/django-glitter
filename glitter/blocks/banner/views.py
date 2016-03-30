93a255cb1d79ab5738596866caeb49af6617f791
Alex Tomkins <tomkins@darkzone.net>
2016-03-30 16:05:19 +0100

    More thumbnailing - make sure all images get thumbnailed
    We'll try and give some control over image sizes on these

--- glitter/blocks/banner/views.py
+++ glitter/blocks/banner/views.py
@@ -1,9 +1,14 @@
 from django.template import RequestContext
 from django.template.loader import render_to_string
+from glitter.templates import get_layout
 
 
 def banner_view(block, request, rerender, content_block, block_classes):
     css_classes = ' '.join(block_classes)
+
+    # Find the column
+    layout = get_layout(template_name=content_block.obj_version.template_name)
+    column = layout._meta.columns[content_block.column]
 
     banner_inlines = block.bannerinline_set.select_related('banner__image').all()
 
@@ -13,5 +18,6 @@ def banner_view(block, request, rerender, content_block, block_classes):
         'content_block': content_block,
         'css_classes': css_classes,
         'object': block,
+        'column': column,
         'banner_inlines': banner_inlines,
     }, context_instance=RequestContext(request))
