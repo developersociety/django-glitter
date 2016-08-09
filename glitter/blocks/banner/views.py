d737f099abaad79fd98c498ae8ed1d1df341be7c
martinphellwig <martin.hellwig@gmail.com>
2016-08-09 09:43:51 +0100

    Changed so that the following warning is removed:
    $GLITTER/blocks/banner/views.py:39: RemovedInDjango110Warning: The
    context_instance argument of render_to_string is deprecated.  },
    context_instance=RequestContext(request))

--- glitter/blocks/banner/views.py
+++ glitter/blocks/banner/views.py
@@ -18,22 +18,13 @@ def banner_view(block, request, rerender, content_block, block_classes):
     if block:
         banner_inlines = block.bannerinline_set.select_related('banner__image').all()
         
-#     template_name = 'glitter/blocks/%s.html' % content_block.content_type.model
-#     context = {
-#         'content_block': content_block,
-#         'css_classes': css_classes,
-#         'object': block,
-#         'column': column,
-#         'banner_inlines': banner_inlines} 
-#     rendered = render_to_string(template_name, context, request=request)
-#     return rendered
-
-    return render_to_string((
-        'glitter/blocks/%s.html' % (content_block.content_type.model,),
-    ), {
+    template_name = 'glitter/blocks/%s.html' % content_block.content_type.model
+    context = {
         'content_block': content_block,
         'css_classes': css_classes,
         'object': block,
         'column': column,
-        'banner_inlines': banner_inlines,
-    }, context_instance=RequestContext(request))
+        'banner_inlines': banner_inlines} 
+    rendered = render_to_string(template_name, context, request=request)
+    return rendered
+
