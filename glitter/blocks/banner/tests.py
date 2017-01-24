225311930de910321aa2683cbd1bcd0cb9d8964d
Alex Tomkins <tomkins@darkzone.net>
2017-01-24 11:07:06 +0000

    Minor banner block test fixes
    Use the editor user for self.client and don't bother with a seperate user.

--- glitter/blocks/banner/tests.py
+++ glitter/blocks/banner/tests.py
@@ -5,7 +5,7 @@ from django.contrib.admin.sites import AdminSite
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import get_callable, reverse
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.test.client import RequestFactory
 from django.test import modify_settings, override_settings
 
@@ -22,16 +22,18 @@ class BannerTestCase(TestCase):
     def setUp(self):
         User = get_user_model()
         self.page = Page.objects.create(url='/redactor/', title='Test page')
-
         self.page_content_type = ContentType.objects.get_for_model(Page)
 
-        self.editor = User.objects.create_user(username='banner', password='banner')
+        editor = User.objects.create_superuser(
+            username='editor', email='editor@example.org', password='editor',
+        )
+        self.client.login(username='editor', password='editor')
 
         page_version = Version.objects.create(
             content_type=self.page_content_type,
             object_id=self.page.id,
             template_name='glitter/sample.html',
-            owner=self.editor
+            owner=editor,
         )
         self.content_block_without_obj = ContentBlock.objects.create(
             obj_version=page_version,
@@ -64,11 +66,6 @@ class BannerTestCase(TestCase):
         self.inline = BannerInline.objects.create(
             banner_block=self.banner_block, banner=self.banner)
 
-        self.super_user = User.objects.create_superuser('test',
-                                                        'test@test.com', 'test')
-        self.super_user_client = Client()
-        self.super_user_client.login(username='test', password='test')
-
     def test_view_without_block(self):
         self.view(
             None, self.request, False, self.content_block_without_obj, 'test-class'
@@ -93,17 +90,17 @@ class BannerTestCase(TestCase):
     def test_banner_view_as_request(self):
         url = reverse('admin:glitter_banner_banner_change',
                       args=(self.banner.id,))
-        response = self.super_user_client.get(url, follow=True)
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_bannerblock_view_as_request(self):
         url = reverse('admin:glitter_banner_banner_change',
                       args=(self.banner.id,))
-        response = self.super_user_client.get(url, follow=True)
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_admin_bannerblock_change(self):
         url = reverse('block_admin:glitter_banner_bannerblock_change',
                       args=(self.banner_block.id,))
-        response = self.super_user_client.get(url, follow=True)
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
