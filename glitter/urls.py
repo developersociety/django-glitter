ed50dd0a74d8ee82410d124e1152064cc3f07fa5
Tom Smith <tom@takeontom.com>
2017-01-27 15:31:58 +0000

    Don't error out when using App Page routes not yet in nav
    For #69

--- glitter/urls.py
+++ glitter/urls.py
@@ -7,6 +7,7 @@ from glitter.integration import glitter_app_pool
 import importlib
 
 urlpatterns = []
+used_apps = []
 
 try:
     # Attempt to find all Glitter App Pages, get their corresponding Glitter App configs and then
@@ -23,6 +24,21 @@ try:
 
             app_url = url(
                 '^{}/'.format(app_page.url.strip('/')),
+                include(app_url_conf, namespace=glitter_app.namespace)
+            )
+            urlpatterns.append(app_url)
+            used_apps.append(app_page.glitter_app_name)
+
+    # If a page has not been created for an app yet, we don't want a NoReverseMatch error every
+    # time someone tries to '{% url %}' or 'reverse()' a viewname. So lets add a URL pattern entry
+    # to support those requests.
+    glitter_apps = glitter_app_pool.get_glitter_apps()
+    for system_name, glitter_app in glitter_apps.items():
+        if system_name not in used_apps:
+            app_url_conf = importlib.import_module(glitter_app.url_conf)
+
+            app_url = url(
+                '^{}/'.format(system_name),
                 include(app_url_conf, namespace=glitter_app.namespace)
             )
             urlpatterns.append(app_url)
