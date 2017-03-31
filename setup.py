d2bb03878a0b1b87d97077f7e2087e025f70b79f
Alex Tomkins <tomkins@darkzone.net>
2017-03-31 12:24:00 +0100

    Version 0.1.10

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 
 setup(
     name='django-glitter',
-    version='0.1.9',
+    version='0.1.10',
     description='Glitter for Django',
     long_description=open('README.rst').read(),
     url='https://github.com/blancltd/django-glitter',
