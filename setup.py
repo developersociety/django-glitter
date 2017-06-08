728a4d159c3ed4b95995d5e0e8a2153f7595b739
Alex Tomkins <tomkins@darkzone.net>
2017-06-08 13:34:52 +0100

    Bump version: 0.1 → 0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires = [
 
 setup(
     name='django-glitter',
-    version='0.1',
+    version='0.2',
     description='Glitter for Django',
     long_description=open('README.rst').read(),
     url='https://github.com/blancltd/django-glitter',
